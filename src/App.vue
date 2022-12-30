1e38ac640dbc6687b08c275830b57e7db6ac9811
Zheheng Zhang <76390451+zhangzheheng12345@users.noreply.github.com>
2022-12-30 17:32:57 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <a href="https://vuejs.org">Vue3 Official</a>
-  <a href="https://vue3js.cn">Vue3 Official (Chinese)</a>
+  <details open>
+    <summary>Frontend</summary>
+    <details open>
+      <summary>Vue</summary>
+      <a href="https://vuejs.org">Vue3 Official</a>
+      <br/>
+      <a href="https://vue3js.cn">Vue3 Official (Chinese)</a>
+    </details>
+  </details>
 </template>
 
 <script lang="ts" setup>
